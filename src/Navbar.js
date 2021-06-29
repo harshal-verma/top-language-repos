61f06eaee6dd4daab397c5639a293d9e84845152
harshal-verma <81582281+harshal-verma@users.noreply.github.com>
2021-06-29 23:44:28 +0530

    initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { FaGithubAlt, FaGitAlt } from "react-icons/fa";
+import "./App.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <a href="/" className="navbar-link">
+        <FaGithubAlt className="navbar-icons"/>
+        <h2 className="navbar-heading">TopRepos</h2>
+      </a>
+      <a href="/" className="navbar-link">
+        <FaGitAlt className="navbar-icons"/>
+        <h2 className="navbar-heading">Fork</h2>
+      </a>
+    </nav>
+  );
+};
+
+export default Navbar;
